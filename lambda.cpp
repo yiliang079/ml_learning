74a86c9577f6f677d6162fba61202cb4063f7d53
yiliang079 <292359029@qq.com>
2026-03-23 21:02:54 +0800

    case update

--- lambda.cpp
+++ lambda.cpp
@@ -4,6 +4,11 @@
 #include <cstdint>
 
 int main() {
+
+
+//--------------------------------------------------------------------------
+//test1
+
     std::vector<uint64_t> addresses = {0x1001, 0x2000, 0x3005, 0x4002};
     uint64_t offset = 0x100; // 外部变量
 
@@ -23,6 +28,25 @@ int main() {
     for (const auto& a : addresses) {
         std::cout << "  0x" << std::hex << a << std::endl;
     }
+//--------------------------------------------------------------------------
+//test2
+std::vector<int> latencies = {300, 700, 450, 1200, 100};
+    int threshold = 500;
+    int count = 0;
+
+    // 使用 Lambda 配合 std::count_if
+    // [&] 表示引用捕获 threshold 和 count
+    count = std::count_if(latencies.begin(), latencies.end(), [&](int l) {
+        return l > threshold; 
+    });
+
+    std::cout << "High latency packets: " << count << std::endl;
+
+    // 顺便排个序
+    std::sort(latencies.begin(), latencies.end(), [](int a, int b) {
+        return a < b; // 从小到大
+    });
+
 
     return 0;
 }
