a0aec108bbb4d6db4d310636a67fe3485d3f00c4
梁漪 <limx79@mail2.sysu.edu.cn>
2026-03-28 18:34:22 +0800

    llm studying

--- llm_learning.py
+++ llm_learning.py
@@ -1,18 +1,30 @@
 import time
+import fitz  # PyMuPDF
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import torch
 from modelscope import snapshot_download
 import os
+
+def extract_text_from_pdf(pdf_path):
+    doc = fitz.open(pdf_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
+
+
 os.environ['HF_HOME'] = "E:/huggingface_cache"
 os.environ['HUGGINGFACE_HUB_CACHE'] = "E:/huggingface_cache"
 
 from transformers import pipeline
-# 明确指定设备：如果有显卡用 "cuda"，没有就用 "cpu"
 device = 0 if torch.cuda.is_available() else -1
 MODEL_ID = "Qwen/Qwen2.5-7B-Instruct"
 custom_path = "E:/huggingface_cache"
 CUSTOM_CACHE_DIR = "E:/huggingface_cache"
 local_dir = snapshot_download(MODEL_ID, cache_dir=CUSTOM_CACHE_DIR)
+TARGET_STEP = 20    # <--- 报错就是因为缺了这一行
+WORDS_PER_STEP = 50 # <--- 这一行通常也在一起
+
 
 print("正在加载模型...")
 start_load = time.time()
@@ -25,6 +37,12 @@ generator = pipeline(
     model_kwargs={"cache_dir": custom_path} 
 )
 
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True
+)
 
 print(f"\n正在以 4-bit 量化模式加载模型到 2080 Ti...")
 tokenizer = AutoTokenizer.from_pretrained(local_dir)
@@ -40,13 +58,12 @@ print("模型加载完成。")
 # ==========================================
 
 # 构造一个需要初步思考的长 Prompt（触发 Prefill）
-prompt = "请详细阐述大语言模型中 MoE 架构（混合专家模型）的物理硬件挑战，特别是在华为 CloudMatrix384 这种 384 节点全互联集群中，路由（Routing）导致的 All-to-All 通信瓶颈、显存碎片化管理以及 P2P 调度死锁的验证策略。请撰写一篇深度技术报告，字数要求在 5000 字以上，包含具体的 SVA（SystemVerilog Assertion）伪代码例子和负载均衡仲裁算法的分析。"
+prompt = "介绍一下Rdma技术的原理和应用场景。" * 10 # 通过重复来增加 Token 数量
 inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
 prompt_tokens = inputs.input_ids.shape[1]
 
 print(f"\n[实验开始]")
 print(f"Prompt Token 数: {prompt_tokens} (即将触发 Prefill)")
-print("请在 nvitop 窗口观察功耗 (Power) 的瞬间飙升。")
 time.sleep(2) # 给你留出看 nvitop 的时间
 
 print("\n--- 正在进行 Prefill (生成第1个词) ---")
@@ -86,31 +103,4 @@ for step in range(TARGET_STEP):
 print("\n[实验完成]")
 print("模型生成的文本片段：\n")
 # 打印最后 200 个词看看效果
-print(tokenizer.decode(generated_ids[0][-200:], skip_special_tokens=True))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print(f"模型加载耗时: {time.time() - start_load:.2f} 秒")
-
-# prompt = "What is china"
-
-# print("正在生成回答...")
-# start_gen = time.time()
-# # 加上 clean_up_tokenization_spaces 让输出更干净
-# result = generator(prompt, max_new_tokens=200, do_sample=True, temperature=0.7)
-# print(f"推理计算耗时: {time.time() - start_gen:.2f} 秒")
-
-# print("\n结果如下：")
-# print(result[0]["generated_text"])
+print(tokenizer.decode(generated_ids[0][-1000:], skip_special_tokens=True))
